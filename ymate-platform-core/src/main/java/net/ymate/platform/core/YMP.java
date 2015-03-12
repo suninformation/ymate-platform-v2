8eff6572c7d4e44c13319301e13d840627b42e77
suninformation <suninformation@163.com>
2015-03-13 01:24:51 +0800

    调整工程结构并在打包时添加版本相关信息

--- ymate-platform-core/src/main/java/net/ymate/platform/core/YMP.java
+++ ymate-platform-core/src/main/java/net/ymate/platform/core/YMP.java
@@ -41,7 +41,7 @@ import java.util.*;
  */
 public class YMP {
 
-    public static final String VERSION = "2.0.0";
+    public static final Version VERSION = new Version(2, 0, 0);
 
     private static final String __YMP_BASE_PACKAGE = "net.ymate.platform";
 
