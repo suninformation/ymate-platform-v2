383e7368740d695d923f505aa81a01b518b0f76d
suninformation <suninformation@163.com>
2018-01-06 04:33:32 +0800

    调整IConfiguration使其继承IConfigReader接口

--- ymate-platform-configuration/src/main/java/net/ymate/platform/configuration/IConfiguration.java
+++ ymate-platform-configuration/src/main/java/net/ymate/platform/configuration/IConfiguration.java
@@ -15,6 +15,8 @@
  */
 package net.ymate.platform.configuration;
 
+import net.ymate.platform.core.support.IConfigReader;
+
 import java.util.List;
 import java.util.Map;
 
@@ -24,92 +26,19 @@ import java.util.Map;
  * @author 刘镇 (suninformation@163.com) on 2011-8-27 上午12:20:51
  * @version 1.0
  */
-public interface IConfiguration {
-
-    /**
-     * @param key 属性键
-     * @return 获得对应的文字值
-     */
-    String getString(String key);
-
-    String getString(String key, String defaultValue);
+public interface IConfiguration extends IConfigReader {
 
     String getString(String category, String key, String defaultValue);
 
-    /**
-     * @param key 属性键
-     * @return 获得对应的文字值列表，其中匹配以key开头的键串
-     */
-    List<String> getList(String key);
-
     List<String> getList(String category, String key);
-
-    /**
-     * @param keyHead 键头标识
-     * @return 获取键值映射
-     */
-    Map<String, String> getMap(String keyHead);
 
     Map<String, String> getMap(String category, String keyHead);
 
-    /**
-     * @param key 属性键
-     * @return 获取键值数组值
-     */
-    String[] getArray(String key);
-
-    String[] getArray(String key, boolean zeroSize);
-
     String[] getArray(String category, String key, boolean zeroSize);
-
-    /**
-     * @param key 属性键
-     * @return 获得对应的数字值
-     */
-    int getInt(String key);
-
-    int getInt(String key, int defaultValue);
 
     int getInt(String category, String key, int defaultValue);
 
-    /**
-     * @param key 属性键
-     * @return 获得对应的布尔值
-     */
-    boolean getBoolean(String key);
-
-    boolean getBoolean(String key, boolean defaultValue);
-
     boolean getBoolean(String category, String key, boolean defaultValue);
-
-    /**
-     * @param key 属性键
-     * @return 获取长整数
-     */
-    long getLong(String key);
-
-    long getLong(String key, long defaultValue);
-
-    /**
-     * @param key 属性键
-     * @return 获取浮点数
-     */
-    float getFloat(String key);
-
-    float getFloat(String key, float defaultValue);
-
-    /**
-     * @param key 属性键
-     * @return 获取双精度浮点数
-     */
-    double getDouble(String key);
-
-    double getDouble(String key, double defaultValue);
-
-    /**
-     * @return 获得配置对象内部加载的配置项映射
-     */
-    Map<String, String> toMap();
 
     Map<String, String> toMap(String category);
 
@@ -117,14 +46,6 @@ public interface IConfiguration {
      * @return 获取分类的名称集合
      */
     List<String> getCategoryNames();
-
-    /**
-     * 判断键key的配置项是否存在
-     *
-     * @param key 属性键
-     * @return 如果存在配置项那么返回true，否则返回false
-     */
-    boolean contains(String key);
 
     boolean contains(String category, String key);
 
