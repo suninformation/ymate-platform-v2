dc9bbe16f97822e3d5024c72dc97b9503b3cecb3
suninformation <suninformation@163.com>
2018-06-11 10:57:47 +0800

    修正Freemarker配置对象初始化后未赋值全局变量

--- ymate-platform-webmvc/src/main/java/net/ymate/platform/webmvc/view/impl/FreemarkerView.java
+++ ymate-platform-webmvc/src/main/java/net/ymate/platform/webmvc/view/impl/FreemarkerView.java
@@ -83,9 +83,9 @@ public class FreemarkerView extends AbstractView {
             try {
                 FreemarkerConfigBuilder _builder = FreemarkerConfigBuilder.create();
                 if (__baseViewPath.startsWith("/WEB-INF")) {
-                    _builder.addTemplateFileDir(new File(RuntimeUtils.getRootPath(), StringUtils.substringAfter(__baseViewPath, "/WEB-INF/"))).build();
+                    __freemarkerConfig = _builder.addTemplateFileDir(new File(RuntimeUtils.getRootPath(), StringUtils.substringAfter(__baseViewPath, "/WEB-INF/"))).build();
                 } else {
-                    _builder.addTemplateFileDir(new File(__baseViewPath)).build();
+                    __freemarkerConfig = _builder.addTemplateFileDir(new File(__baseViewPath)).build();
                 }
             } catch (IOException e) {
                 throw new Error(RuntimeUtils.unwrapThrow(e));
