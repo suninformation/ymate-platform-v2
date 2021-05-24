21ebe75cd39b37120cae19baf375b69f3cddaf40
suninformation <suninformation@163.com>
2021-05-24 12:03:40 +0800

    优化调整配置注入逻辑对基础类型、数组、集合和映射的支持

--- ymate-platform-configuration/src/main/java/net/ymate/platform/configuration/support/ConfigValueInjector.java
+++ ymate-platform-configuration/src/main/java/net/ymate/platform/configuration/support/ConfigValueInjector.java
@@ -22,12 +22,16 @@ import net.ymate.platform.configuration.annotation.Configs;
 import net.ymate.platform.core.beans.IBeanFactory;
 import net.ymate.platform.core.beans.IBeanInjector;
 import net.ymate.platform.core.configuration.IConfigFileParser;
+import net.ymate.platform.core.configuration.IConfigReader;
 import net.ymate.platform.core.configuration.IConfiguration;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
+import java.util.*;
+import java.util.stream.IntStream;
 
 /**
  * @author 刘镇 (suninformation@163.com) on 2019-04-25 13:52
@@ -41,7 +45,6 @@ public class ConfigValueInjector implements IBeanInjector {
         }
         ConfigValue configValueAnn = ((ConfigValue) annotation);
         String categoryStr = configValueAnn.category();
-        String valueStr = null;
         String keyStr = StringUtils.defaultIfBlank(configValueAnn.value(), field.getName());
         Class<? extends IConfiguration>[] configs = configValueAnn.configs();
         if (ArrayUtils.isEmpty(configs) || StringUtils.isBlank(categoryStr)) {
@@ -66,24 +69,114 @@ public class ConfigValueInjector implements IBeanInjector {
                 }
             }
         }
+        Object returnValue = null;
         categoryStr = StringUtils.defaultIfBlank(categoryStr, IConfigFileParser.DEFAULT_CATEGORY_NAME);
         if (ArrayUtils.isNotEmpty(configs)) {
             for (Class<? extends IConfiguration> configurationClass : configs) {
                 IConfiguration configuration = beanFactory.getBean(configurationClass);
                 if (configuration != null) {
-                    valueStr = configuration.getString(categoryStr, keyStr, null);
-                    if (StringUtils.isNotBlank(valueStr)) {
+                    returnValue = parseParam(configuration, field, categoryStr, keyStr);
+                    if (returnValue != null) {
                         break;
                     }
                 }
             }
         }
-        if (StringUtils.isBlank(valueStr)) {
+        if (returnValue == null) {
             if (!StringUtils.equalsIgnoreCase(IConfigFileParser.DEFAULT_CATEGORY_NAME, categoryStr)) {
                 keyStr = categoryStr.concat(".").concat(keyStr);
             }
-            valueStr = beanFactory.getOwner().getParam(keyStr);
+            returnValue = parseParam(beanFactory, field, keyStr, configValueAnn.defaultValue());
         }
-        return BlurObject.bind(StringUtils.defaultIfBlank(valueStr, configValueAnn.defaultValue())).toObjectValue(field.getType());
+        return returnValue;
+    }
+
+    private Object parseParam(IConfiguration configuration, Field field, String categoryStr, String keyStr) {
+        Object returnValue;
+        Class<?> fieldType;
+        boolean isArray = field.getType().isArray();
+        if (isArray) {
+            fieldType = ClassUtils.getArrayClassType(field.getType());
+            String[] valueArray = configuration.getArray(categoryStr, keyStr, false);
+            returnValue = parseArrayValue(valueArray, fieldType);
+        } else {
+            fieldType = field.getType();
+            if (List.class.equals(fieldType)) {
+                returnValue = parseListValue(configuration.getList(categoryStr, keyStr), null);
+            } else if (Map.class.equals(fieldType)) {
+                returnValue = parseMapValue(configuration.getMap(categoryStr, keyStr), null);
+            } else {
+                returnValue = BlurObject.bind(configuration.getString(categoryStr, keyStr)).toObjectValue(field.getType());
+            }
+        }
+        return returnValue;
+    }
+
+    private Object parseParam(IBeanFactory beanFactory, Field field, String keyStr, String defaultValue) {
+        IConfigReader configReader = beanFactory.getOwner().getParamConfigReader();
+        Object returnValue;
+        Class<?> fieldType;
+        boolean isArray = field.getType().isArray();
+        if (isArray) {
+            fieldType = ClassUtils.getArrayClassType(field.getType());
+            String[] valueArray = configReader.getArray(keyStr, StringUtils.split(defaultValue, "|"));
+            returnValue = parseArrayValue(valueArray, fieldType);
+        } else {
+            fieldType = field.getType();
+            if (List.class.equals(fieldType)) {
+                returnValue = parseListValue(configReader.getList(keyStr), defaultValue);
+            } else if (Map.class.equals(fieldType)) {
+                returnValue = parseMapValue(configReader.getMap(keyStr), defaultValue);
+            } else {
+                returnValue = BlurObject.bind(configReader.getString(keyStr, defaultValue)).toObjectValue(field.getType());
+            }
+        }
+        return returnValue;
+    }
+
+    private Object[] parseArrayValue(String[] valueArray, Class<?> fieldType) {
+        if (valueArray != null) {
+            if (Objects.equals(fieldType, String.class)) {
+                return valueArray;
+            } else {
+                Object[] newArray = (Object[]) Array.newInstance(fieldType, valueArray.length);
+                IntStream.range(0, valueArray.length).forEach(idx -> newArray[idx] = BlurObject.bind(valueArray[idx]).toObjectValue(fieldType));
+                return newArray;
+            }
+        }
+        return null;
+    }
+
+    private List<String> parseListValue(List<String> valueList, String defaultValue) {
+        if (valueList == null || valueList.isEmpty()) {
+            if (StringUtils.isBlank(defaultValue)) {
+                return null;
+            }
+            return Arrays.asList(StringUtils.split(StringUtils.trimToEmpty(defaultValue), "|"));
+        }
+        return valueList;
+    }
+
+    private Map<String, String> parseMapValue(Map<String, String> valueMap, String defaultValue) {
+        if (valueMap == null || valueMap.isEmpty()) {
+            if (StringUtils.isBlank(defaultValue)) {
+                return null;
+            }
+            String[] mapItems = StringUtils.split(StringUtils.trimToEmpty(defaultValue), "|");
+            if (ArrayUtils.isNotEmpty(mapItems)) {
+                if (valueMap == null) {
+                    valueMap = new HashMap<>(mapItems.length);
+                }
+                for (String mapItem : mapItems) {
+                    String[] itemArray = StringUtils.split(mapItem, ":");
+                    if (itemArray != null && itemArray.length == 2) {
+                        valueMap.put(itemArray[0], itemArray[1]);
+                    }
+                }
+                return valueMap;
+            }
+            return Collections.emptyMap();
+        }
+        return valueMap;
     }
 }
