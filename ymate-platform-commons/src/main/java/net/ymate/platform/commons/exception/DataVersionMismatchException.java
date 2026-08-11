5283d4fd4f3daad0fb53ec136ba86a1a68120830
suninformation <suninformation@163.com>
2026-08-11 21:02:35 +0800

    优化版本比较逻辑支持泛型及完善多种类型的比较规则

--- ymate-platform-commons/src/main/java/net/ymate/platform/commons/exception/DataVersionMismatchException.java
+++ ymate-platform-commons/src/main/java/net/ymate/platform/commons/exception/DataVersionMismatchException.java
@@ -23,15 +23,62 @@ package net.ymate.platform.commons.exception;
 public class DataVersionMismatchException extends RuntimeException {
 
     /**
-     * 版本比较，若版本数值不相等则抛出 DataVersionMismatchException 异常
+     * 版本比较，若版本不相等则抛出 DataVersionMismatchException 异常
+     * <p>
+     * 比较规则：
+     * <ul>
+     *     <li>两个版本均为 null 时视为相等，不抛出异常；</li>
+     *     <li>仅一方为 null 时视为不相等，抛出异常；</li>
+     *     <li>类型相同时，若两者均实现了 {@link Comparable} 且可相互比较则优先使用 {@code compareTo}，
+     *     否则回退到 {@link Object#equals}；</li>
+     *     <li>类型不同但均为数值类型时，按数值大小进行比较，规避 {@code Long} 与 {@code Integer}
+     *     等因类型不一致导致 {@code equals} 返回 false 的问题；</li>
+     *     <li>其它类型不一致的场景，始终视为不相等。</li>
+     * </ul>
      *
      * @param originVersion  原始版本(持有者拥有的版本)
      * @param currentVersion 当前版本(数据最新版本)
      */
-    public static void comparisonVersion(Long originVersion, Long currentVersion) {
-        if (currentVersion != null && originVersion != null && !originVersion.equals(currentVersion)) {
-            throw new DataVersionMismatchException(String.format("Data version mismatch. origin: %d, current: %d", originVersion, currentVersion));
+    @SuppressWarnings("unchecked")
+    public static <T> void comparisonVersion(T originVersion, T currentVersion) {
+        // 两者均为 null 视为相等
+        if (originVersion == null && currentVersion == null) {
+            return;
         }
+        // 仅一方为 null，版本不匹配
+        if (originVersion == null || currentVersion == null) {
+            doThrowMismatch(originVersion, currentVersion);
+        }
+        // 类型相同
+        if (originVersion.getClass().equals(currentVersion.getClass())) {
+            if (originVersion instanceof Comparable) {
+                try {
+                    if (((Comparable<Object>) originVersion).compareTo(currentVersion) != 0) {
+                        doThrowMismatch(originVersion, currentVersion);
+                    }
+                    return;
+                } catch (ClassCastException ignored) {
+                    // 类型相同但不兼容 Comparable（理论上不会发生），回退到 equals
+                }
+            }
+            if (!originVersion.equals(currentVersion)) {
+                doThrowMismatch(originVersion, currentVersion);
+            }
+            return;
+        }
+        // 类型不同但均为数值类型，按数值比较
+        if (originVersion instanceof Number && currentVersion instanceof Number) {
+            if (((Number) originVersion).longValue() != ((Number) currentVersion).longValue()) {
+                doThrowMismatch(originVersion, currentVersion);
+            }
+            return;
+        }
+        // 类型不同且无法比较，视为版本不匹配
+        doThrowMismatch(originVersion, currentVersion);
+    }
+
+    private static <T> void doThrowMismatch(T originVersion, T currentVersion) {
+        throw new DataVersionMismatchException(String.format("Data version mismatch. origin: %s, current: %s", originVersion, currentVersion));
     }
 
     public DataVersionMismatchException() {
